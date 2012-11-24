d3bcad82a9b4f8d40f031e1f9d6dc84eaa2927e4
Eduardo Habkost <ehabkost@raisama.net>
2012-11-24 16:04:41 -0200

    instapaper: look for unread _and_ archived bookmarks
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- syncitlater/sync.py
+++ syncitlater/sync.py
@@ -16,14 +16,18 @@ class InstapaperMember(SyncMember):
 		self.api = api
 		super(InstapaperMember, self).__init__(state)
 
-	def _have(self):
+	def _have(self, folder_id='unread'):
 		"""Generate 'have' argument for instalaper API call"""
 		items = []
 		for i in self.state.get('known_bookmarks', []):
-			items.append('%s:%s' % (i['bookmark_id'], i['hash']))
+			if i.get('folder_id', 'unread') == folder_id:
+				items.append('%s:%s' % (i['bookmark_id'], i['hash']))
 		return ','.join(items)
 
 	def get_changes(self):
-		bookmarks = self.api.list_bookmarks(have=self._have())
-		for b in bookmarks:
+		unread = self.api.list_bookmarks(have=self._have('unread'), folder_id='unread')
+		for b in unread:
 			yield dict(url=b['url'], state='unread')
+		archived = self.api.list_bookmarks(have=self._have('archive'), folder_id='archive')
+		for b in archived:
+			yield dict(url=b['url'], state='archived')
--- syncitlater/tests/instapaper_sync.py
+++ syncitlater/tests/instapaper_sync.py
@@ -4,8 +4,8 @@ import unittest
 
 
 class FakeInstapaperApi:
-	def list_bookmarks(self, **kwargs):
-		return self.fake_bookmarks
+	def list_bookmarks(self, folder_id='unread', **kwargs):
+		return self.fake_bookmarks.get(folder_id, [])
 
 class InstapaperSyncTest(unittest.TestCase):
 	def setUp(self):
@@ -22,17 +22,61 @@ class InstapaperSyncTest(unittest.TestCase):
 			dict(bookmark_id=200, hash='2hundred')]
 		self.assertEquals(self.member._have(), '100:hundred,200:2hundred')
 
+	def testSomeKnownPerFolder(self):
+		self.state['known_bookmarks'] = [
+			dict(bookmark_id=100, hash='hundred0', folder_id='unread'),
+			dict(bookmark_id=200, hash='2hundred0', folder_id='unread'),
+			dict(bookmark_id=101, hash='hundred1', folder_id='archive'),
+			dict(bookmark_id=201, hash='2hundred1', folder_id='archive')]
+		self.assertEquals(self.member._have('unread'), '100:hundred0,200:2hundred0')
+		self.assertEquals(self.member._have('archive'), '101:hundred1,201:2hundred1')
+
 	def testGetChanges(self):
 		self.state['known_bookmarks'] = [
 			dict(bookmark_id=1, hash='one'),
 			dict(bookmark_id=2, hash='two')]
-		self.api.fake_bookmarks = [
+		self.api.fake_bookmarks = {'unread':[
 			dict(type='bookmark', bookmark_id=3, hash='three', url='http://3.example.com/'),
 			dict(type='bookmark', bookmark_id=4, hash='four', url='http://4.example.com/'),
-		]
+		]}
 		changes = list(self.member.get_changes())
 		self.assertEquals(len(changes), 2)
 		self.assertEquals(changes[0]['url'], 'http://3.example.com/')
 		self.assertEquals(changes[0]['state'], 'unread')
 		self.assertEquals(changes[1]['url'], 'http://4.example.com/')
 		self.assertEquals(changes[1]['state'], 'unread')
+
+	def testGetMultifolderChanges(self):
+		self.state['known_bookmarks'] = [
+			dict(bookmark_id=1, hash='one'),
+			dict(bookmark_id=2, hash='two'),
+			dict(bookmark_id=3, hash='three'),
+			dict(bookmark_id=4, hash='four'),
+			]
+		self.api.fake_bookmarks = {
+			'unread':[
+				dict(type='bookmark', bookmark_id=5, hash='five', url='http://5.example.com/'),
+				dict(type='bookmark', bookmark_id=6, hash='six', url='http://6.example.com/'),
+				dict(type='bookmark', bookmark_id=3, hash='six', url='http://3.example.com/'), # moved from archive to unread
+			],
+			'archive':[
+				dict(type='bookmark', bookmark_id=7, hash='five', url='http://7.example.com/'),
+				dict(type='bookmark', bookmark_id=8, hash='six', url='http://8.example.com/'),
+				dict(type='bookmark', bookmark_id=2, hash='six', url='http://2.example.com/'), # moved from unread to archive
+			]}
+
+		changes = list(self.member.get_changes())
+		self.assertEquals(len(changes), 6)
+
+		self.assertEquals(changes[0]['url'], 'http://5.example.com/')
+		self.assertEquals(changes[0]['state'], 'unread')
+		self.assertEquals(changes[1]['url'], 'http://6.example.com/')
+		self.assertEquals(changes[1]['state'], 'unread')
+		self.assertEquals(changes[2]['url'], 'http://3.example.com/')
+		self.assertEquals(changes[2]['state'], 'unread')
+		self.assertEquals(changes[3]['url'], 'http://7.example.com/')
+		self.assertEquals(changes[3]['state'], 'archived')
+		self.assertEquals(changes[4]['url'], 'http://8.example.com/')
+		self.assertEquals(changes[4]['state'], 'archived')
+		self.assertEquals(changes[5]['url'], 'http://2.example.com/')
+		self.assertEquals(changes[5]['state'], 'archived')
